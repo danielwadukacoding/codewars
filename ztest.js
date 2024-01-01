7d186c52a34532bd95871039dbfd3ba9bbbcc5c7
wadsdaniel <danielwaduka@gmail.com>
2024-01-01 05:54:53 +0300

    Removed the test files

---
+++ ztest.js
@@ -0,0 +1,13 @@
+// let num = 4;
+
+function squareOfThree(){
+    num = 3;
+    return num * num;
+}
+
+function squareOfFour(){
+    return num * num;
+}
+
+console.log(`The square of 3 is ${squareOfThree()}`);
+console.log(`The square of 4 is ${squareOfFour()}`);
